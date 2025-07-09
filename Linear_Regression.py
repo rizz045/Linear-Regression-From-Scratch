9f333d1ebb6ea7b48c7baf82913233034db38fa0
Rishabh Kumar Dhirhe <kdrishabhkd@gmail.com>
2025-07-09 22:49:55 +0530

    Update Linear_Regression.py

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -1,1 +1,10 @@
 import numpy as np
+
+# Creating in class template
+
+class Linear_Regression():
+
+    #initiating parameters (hyper parameters: learning_rate and no_of_iteration)
+    def __init__(self, learning_rate, no_of_iterations):
+        self.learning_rate = learning_rate
+        self.no_of_iterations = no_of_iterations
